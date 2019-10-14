473fe06449a5c0b623fe1fcd822639f147100003
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-14 18:57:28 -0400

    mvp complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from "react";
+import * as rtl from "react-testing-library";
+import { render, fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+afterEach(rtl.cleanup);
+describe("Control", () => {
+  it("it rendered correctly", () => {
+    const wrapper = rtl.render(<Controls />);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+
+  it("it changes to open gate correctly", () => {
+    const { getByText, findByText } = render(<Controls />);
+    fireEvent.click(getByText("Close Gate"));
+    findByText(/open gate/i);
+  });
+
+  it("it changes to unlock gate correctly", () => {
+    const { getByText, findByText } = render(<Controls />);
+    fireEvent.click(getByText("Lock Gate"));
+    findByText(/unlock gate/i);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from "react";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+afterEach(rtl.cleanup);
+describe("Display", () => {
+  it("it rendered correctly", () => {
+    const wrapper = rtl.render(<Display />);
+    const hasOpen = wrapper.queryByText(/open/i);
+    const hasUnlocked = wrapper.queryByText(/unlocked/i);
+
+    expect(wrapper.baseElement).toMatchSnapshot();
+    expect(hasOpen).toBeInTheDocument();
+    expect(hasUnlocked).toBeInTheDocument();
+  });
+});
